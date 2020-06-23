b59926bb1707ab82c9012cb9360ce015ec299d6b
KimSH39 <63898043+KimSH39@users.noreply.github.com>
2020-06-24 00:49:53 +0900

    Update 멋사 파이썬 과제.py

--- 멋사 파이썬 과제.py
+++ 멋사 파이썬 과제.py
@@ -1,9 +1,10 @@
-weight = int(input("체중을 입력하세요: "))
-tall = int(input("키(cm)를 입력하세요: "))
-tall2= tall/100
-bmi = weight/tall2**2
+weight, tall = input("몸무게(kg)와 키(cm) 입력: ").split()
 
-if bmi >= 20.0 and bmi <25:
+tall=float(tall)
+weight=float(weight)
+bmi = weight/(tall*0.01)**2
+
+if bmi >= 20 and bmi <25:
     print("표준입니다.")
 else:
     print("체중관리가 필요합니다.")
